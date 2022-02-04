e92b2e5d6039bac46221ff77f148aa62cb3be710
Emircan GÜLMEZ <98878744+EmircanGulmez@users.noreply.github.com>
2022-02-04 09:59:01 +0300

    Add files via upload

---
+++ zar_atma.py
@@ -0,0 +1,12 @@
+# 2 Adet Zar Atma
+import random
+while 1:
+    karar = input("\nZar atılsın mı? (e/h) : ")
+    if karar == 'e':
+        zar1 = random.randint(1,6)
+        zar2 = random.randint(1,6)
+        print(f"Zar sonucu : {zar1} / {zar2}")
+    elif karar == 'h':
+        break
+    else:
+        print("Zar atılamadı...")
